f048731c7f95b9bc6068e103e2bf8552aa395998
OrdinaryAzazel <19622052@mahasiswa.itb.ac.id>
2023-11-26 04:22:37 +0700

    update

--- ma.py
+++ ma.py
@@ -37,7 +37,7 @@ class PDA:
                     i += 1
             else:
                 i += 1
-        print(slices_list)
+        # print(slices_list)
         for symbol in slices_list:
             match=re.match(r'<(.*?)>', symbol)
             extracted_content = match.group() if match else None
@@ -47,16 +47,14 @@ class PDA:
             list_att=shlex.split(attributes)
             current_stack_top = self.stack[-1] if self.stack else None
             transition = self.find_transition(current_state, tag_name, current_stack_top)
-            print(tag_name)
+            # print(tag_name)
             if transition is None:
-                print("salah di1")
-                print(tag_name)
+                # print(f"Salah di : {tag_name}\n")
                 return False
             next_state, stack_action, stackkaa = transition
             current_state = next_state
             if extracted_content!=symbol and current_state!='stringstate':
-                print("Salah di:")
-                print(symbol)
+                print(f"Salah di : {symbol}\n")
                 return False
             if stackkaa != 'e' and stackkaa==current_stack_top:
                 self.stack.pop()
@@ -77,43 +75,48 @@ class PDA:
                 type = ['text','password','email','number','checkbox']
             elif (tag_name == '<button>'):
                 type = ['submit','reset','button']
-            print(list_att)
+            # print(list_att)
             if (len(listofwajib)!=0):
                 
                 for elements in list_att:
-                    print(elements)
-                    print(listofwajib)
+                    # print(elements)
+                    # print(listofwajib)
                     i=0
                     end_index = elements.find('=', i)
                     current_slice = elements[i:end_index]
-                    print(current_slice)
+                    # print(current_slice)
                     if (current_slice not in listofwajib) and (current_slice not in listofh):
-                        print("masuk wajib\n")
+                        print(f"Salah di : {current_slice}\n")
                         return False
                     elif current_slice in listofwajib:
-                        print("masuk wajib ini\n")
+                       
                         listofwajib.remove(current_slice)
                     elif current_slice in listofh:
-                        print("masuk wajib itu\n")
                         listofh.remove(current_slice)
                 if len(listofwajib)!=0:
+                    print(f"Salah di : {listofwajib}\n")
+                    
                     return False
             elif (len(list_att)!=0):
-                print("masuk sini\n")
+                # print("masuk sini\n")
                 for elements in list_att:
                     i=0
                     end_index = elements.find('=', i)
                     current_slice = elements[i:end_index]
                     end_filled = elements.find('"',end_index+2)
                     filled_slice=elements[end_index+2:end_filled]
-                    print(filled_slice)
+                    # print(filled_slice)
                     if current_slice not in listofwajib and (current_slice not in listofh):
+                        print(f"Salah di : {current_slice}\n")
                         return False
                     elif current_slice in listofwajib:
                         listofwajib.remove(current_slice)
                     elif current_slice in listofh:
-                        if filled_slice not in type:
-                            return False
+                        if current_slice in ['type','method']:
+                            if filled_slice not in type:
+                                print(f"Salah di : {filled_slice}\n")
+                                # print("masuk sini 3\n")
+                                return False
                 
 
                 
@@ -167,14 +170,14 @@ if __name__ == "__main__":
         with open(html_file, 'r') as file:
             html_content = file.read()
 
-    # Remove newlines and spaces
+    
         html_content_stripped = html_content.replace('\n', ' ')
     except FileNotFoundError:
         print(f"HTML file {html_file} not found.")
         sys.exit(1)
 
     pda = PDA(states, input_symbols, stack_symbols, start_state, start_stack, accepting_states, transitions)
-    print(html_content_stripped)
+
     if pda.process_input(html_content_stripped):
         print("Accepted")
     else:
